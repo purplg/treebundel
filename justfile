7a945c715a97d5a2f992228006fb793476ef63f5
Ben Whitley <dev@purplg.com>
2024-04-29 16:31:40 -0400

    Refactored tests setup and justfile for better Eask docker support

--- justfile
+++ justfile
@@ -1,11 +1,30 @@
-test pattern="":
-    eask exec buttercup -L . --traceback pretty --only-error --pattern "{{ pattern }}"
+ALL_VERSIONS := "27.1 27.2 28.1 28.2 29.1 29.2 29.3 master"
 
-lintaf:
-    eask lint checkdoc
-    eask lint declare
-    eask lint elint
-    eask lint indent
-    eask lint keywords
-    eask lint package
-    eask lint regexps
+install *versions="master":
+    for version in {{ versions }}; do \
+      eask docker $version install-deps --dev ; \
+    done
+
+lint *versions="master":
+    for version in {{ versions }}; do \
+      eask docker ${version} lint checkdoc ; \
+      eask docker ${version} lint declare ; \
+      eask docker ${version} lint elint ; \
+      eask docker ${version} lint indent ; \
+      eask docker ${version} lint keywords ; \
+      eask docker ${version} lint package ; \
+    done
+
+_test version:
+    eask docker {{ version }} exec buttercup -L . --traceback pretty
+
+test *versions="master":
+    for version in {{ versions }}; do \
+      just _test ${version} ; \
+    done
+
+install-all:
+    just install {{ ALL_VERSIONS }}
+
+test-all:
+    just test {{ ALL_VERSIONS }}
--- tests/treebundel-tests.el
+++ tests/treebundel-tests.el
@@ -1,4 +1,4 @@
-;;; treebundel-tests.el --- -*- lexical-binding: t; treebundel-workspace-root: "/tmp/treebundel-tests/workspaces"; treebundel-bare-dir: "/tmp/treebundel-tests/workspaces/.bare/" -*-
+;;; treebundel-tests.el --- -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 (require 'treebundel)
@@ -7,9 +7,9 @@
 (defvar treebundel-test-debug nil
   "Set to t to log to a file for debugging tests failures.")
 
-(setopt inhibit-message (not treebundel-test-debug))
+(setq inhibit-message (not treebundel-test-debug))
 (when treebundel-test-debug
-  (setopt inhibit-message nil)
+  (setq inhibit-message nil)
   (defun treebundel--gitlog (type &rest msg)
     "Overridden logging method to write to a file instead of buffer."
     (with-temp-buffer
@@ -26,41 +26,71 @@
 
 (defvar treebundel-tests-dir (file-name-concat temporary-file-directory "treebundel-tests"))
 (defvar treebundel-tests-remotes (file-name-concat treebundel-tests-dir "remote/"))
-(setopt treebundel-workspace-root (file-name-concat treebundel-tests-dir "workspaces/"))
-(setopt treebundel-bare-dir (file-name-concat treebundel-workspace-root ".bare/"))
+(setq treebundel-workspace-root (file-name-concat treebundel-tests-dir "workspaces/"))
+(setq treebundel-bare-dir (file-name-concat treebundel-workspace-root ".bare/"))
 
 (defun treebundel-tests-setup (remote-name branches &optional num-commits)
+  "Prepares a workspace structure for tests.
+A temporary directory structure located at `treebundel-tests-dir' is
+deleted when a single tests completes.
+
+`treebundel-tests-dir'
+  L remotes
+    L branches ; Temporary worktrees used to generate commits on the remotes below.
+    L remote-a.git
+    L remote-b.git
+  L workspaces ; The same workspace structure defined in the main package used for testing. "
+  ;; Use a fake identity for generating commits
+  (setenv "GIT_COMMITTER_NAME" "treebundel")
+  (setenv "GIT_COMMITTER_EMAIL" "fakeuser@treebundel")
+  (setenv "GIT_AUTHOR_NAME" "treebundel")
+  (setenv "GIT_AUTHOR_EMAIL" "fakeuser@treebundel")
+
   (unless (listp branches) (setq branches (list branches)))
-  ;; Ensure remote exists.
   (let ((remote-path (file-name-concat
                       treebundel-tests-remotes
                       (file-name-with-extension remote-name ".git"))))
+    ;; Ensure remote exists.
     (unless (file-exists-p remote-path)
-      (treebundel--git
-        "init" "--bare" remote-path))
+      (treebundel--git "init" "--bare" remote-path))
+    ;; Directory for creating temporary worktrees for generating
+    ;; commits.
     (make-directory (file-name-concat treebundel-tests-remotes "branches" remote-name) t)
+    ;; Create a default branch
+    (let ((repo-path (file-name-concat treebundel-tests-remotes "branches" "main")))
+      (treebundel--git "clone" remote-path repo-path)
+      (treebundel--git-with-repo repo-path "commit" "--allow-empty" "-m" "Initial commit")
+      (treebundel--git-with-repo repo-path "push" "--set-upstream")
+      (delete-directory repo-path t))
+    ;; Create a `treebundel-tests-dir'/remotes/branches/<branch-name>
+    ;; for each branch.
     (dolist (branch-name branches)
-      (unless (file-exists-p (file-name-concat remote-path
-                                               "refs/heads/"
-                                               branch-name))
+      ;; Don't create the branch twice.
+      (unless (file-exists-p (file-name-concat remote-path "refs/heads/" branch-name))
         (let ((repo-path (file-name-concat treebundel-tests-remotes
                                            "branches"
                                            remote-name
                                            branch-name)))
-          (treebundel--git "clone" remote-path repo-path)
-          (treebundel--git-with-repo repo-path "checkout" "-b" branch-name)
+          ;; Make the initial temporary worktree to generate commits in.
+          ;; I would use a worktree for this instead of cloning, but
+          ;; creating orphan branches from a worktree isn't support
+          ;; until git v2.42.0.
+          ;; https://github.com/git/git/commit/35f0383ca61db8f0b6ee1e9002150bbb13649993
+          (treebundel--git-with-repo repo-path "clone" remote-path)
+          ;; Create some commits
           (dotimes (i (or num-commits 3))
             (let ((test-file (file-name-concat repo-path "some-file")))
               (with-temp-buffer (insert i) (write-file test-file))
               (treebundel--git-with-repo repo-path "add" test-file)
               (treebundel--git-with-repo repo-path
-                "commit" "-m" (concat "commit-" (int-to-string i)))))
-          (treebundel--git-with-repo repo-path
-            "push" "--set-upstream" "origin" branch-name)))
-      (delete-directory (file-name-concat treebundel-tests-remotes "branches" branch-name) t)))
+                "commit" "-m" (concat "commit-" (int-to-string i)))))))
+      ;; Push commits up to remote
+      (treebundel--git-with-repo repo-path
+        "push" "--set-upstream" "origin" branch-name)
+      (delete-directory (file-name-concat treebundel-tests-remotes "branches") t)))
   (delete-directory (file-name-concat treebundel-tests-remotes "branches") t))
 
-(describe nil
+(describe "Treebundel"
   :var* ((workspace "workspace-one")
          (remote "remote-a")
          (remote-path (file-name-concat
@@ -122,7 +152,7 @@
     (it "should generate branch name"
       (treebundel--project-add workspace remote)
       (expect (treebundel--git-with-repo (treebundel-project-path workspace
-                                                          remote)
+                                                                  remote)
                 "rev-parse" "--abbrev-ref" "HEAD")
               :to-equal (concat treebundel-branch-prefix workspace)))
 
