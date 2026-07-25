2bd3338277e536cd250b5deebec4282c388f4f83
Ben Whitley <dev@purplg.com>
2026-07-25 17:44:39 -0400

    marginalia

---
+++ treebundel-marginalia.el
@@ -0,0 +1,106 @@
+;;; treebundel-marginalia.el --- Marginalia annotations for treebundel completions -*- lexical-binding: t; -*-
+;;
+;; Package-Requires: ((emacs "30.1") (marginalia "2.11"))
+;; Author: Ben Whitley
+;; Version: 0.0.1
+;; Keywords: convenience docs extensions help vc
+;; Homepage: https://github.com/purplg/treebundel-marginalia
+;; SPDX-License-Identifier: MIT
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+(require 'treebundel)
+(require 'marginalia)
+
+(setq marginalia-command-categories '((projectile-switch-to-buffer . buffer)
+                                      (persp-switch-to-buffer . buffer)
+                                      (flycheck-error-list-set-filter . builtin)
+                                      (imenu . imenu)
+                                      (recentf-open . file)
+                                      (where-is . command)
+                                      (treebundel-bare . treebundel-bare)
+                                      (treebundel-read-bare . treebundel-bare)
+                                      (treebundel-switch-bare . treebundel-bare)
+                                      (treebundel-delete-bare . treebundel-bare)
+                                      (treebundel-project . treebundel-project)
+                                      (treebundel-read-project . treebundel-project)
+                                      (treebundel-switch-project . treebundel-project)
+                                      (treebundel-switch-workspace . treebundel-workspace)))
+
+(defun treebundel-marginalia--bare-annotator (bare)
+  ""
+  (let* ((projects (cdr (treebundel--worktree-list (treebundel--bare-path bare))))
+         (workspaces (mapcar (lambda (project) (treebundel--workspace-of (cadr project)))
+                             projects)))
+    (delete-dups workspaces)
+    (marginalia--fields
+     ((concat
+       (propertize (let ((workspace-count (length workspaces)))
+                     (format "%s" (propertize (format "%d" workspace-count) 'face 'treebundel-workspace)))
+                   :face 'treebundel-workspace)
+       "/"
+       (propertize (let ((project-count (length projects)))
+                     (format "%s" (propertize (format "%d" project-count) 'face 'treebundel-project)))
+                   :face 'treebundel-project))))))
+
+(defun treebundel-marginalia--workspace-annotator-project-count (workspace)
+  ""
+  (when-let* ((projects (treebundel--workspace-projects workspace))
+              ((length> projects 0)))
+    (marginalia--fields
+     ((treebundel--fmt-project (format "%d" (length projects)))))))
+
+(defun treebundel-marginalia--workspace-annotator-project-list (workspace)
+  ""
+  (when-let* ((projects (treebundel--workspace-projects workspace)))
+    (marginalia--fields
+     ((string-join (mapcar (lambda (project) (treebundel--fmt-project project)) projects) ", ")))))
+
+(defun treebundel-marginalia--workspace-annotator-bare-list (workspace)
+  ""
+  (when-let* ((projects (treebundel--workspace-projects workspace))
+              (bares (seq-map (lambda (project) (treebundel--bare-of workspace project)) projects)))
+    (delete-dups bares)
+    (marginalia--fields
+     ((treebundel-marginalia--workspace-annotator-project-count workspace))
+     ((string-join (mapcar (lambda (bare) (treebundel--fmt-bare bare)) bares) ", ")))))
+
+(defun treebundel-marginalia--project-annotator (project)
+  ""
+  (marginalia--fields
+   ((when-let* ((workspace (and (transient-scope) (oref (transient-scope) workspace)))
+                (bare (treebundel--bare-of workspace project)))
+      (treebundel--fmt-bare bare)))))
+
+(defun treebundel-marginalia-enable ()
+  "Enable the treebundel marginalia annotators."
+  (interactive)
+  (add-to-list 'marginalia-annotators '(treebundel-bare treebundel-marginalia--bare-annotator builtin none))
+  (add-to-list 'marginalia-annotators '(treebundel-workspace
+                                        treebundel-marginalia--workspace-annotator-bare-list
+                                        treebundel-marginalia--workspace-annotator-project-list
+                                        treebundel-marginalia--workspace-annotator-project-count
+                                        none))
+  (add-to-list 'marginalia-annotators '(treebundel-project treebundel-marginalia--project-annotator builtin none)))
+
+(defun treebundel-marginalia-disable ()
+  "Disable treebundel marginalia annotators."
+  (interactive)
+  (setq marginalia-annotators (delete '(treebundel-bare treebundel-marginalia--bare-annotator builtin none)
+                                      marginalia-annotators))
+  (setq marginalia-annotators (delete '(treebundel-workspace
+                                        treebundel-marginalia--workspace-annotator-bare-list
+                                        treebundel-marginalia--workspace-annotator-project-list
+                                        treebundel-marginalia--workspace-annotator-project-count
+                                        none)
+                                      marginalia-annotators))
+  (setq marginalia-annotators (delete '(treebundel-project treebundel-marginalia--project-annotator builtin none)
+                                      marginalia-annotators)))
+
+(provide 'treebundel-marginalia)
+;;; treebundel-marginalia.el ends here
